7f5f928c15818db0b48d2a1b5bf35ff10a72656a
MeepoohSUW <99584996+MeepoohSUW@users.noreply.github.com>
2022-02-22 22:55:43 +0700

    create di table

---
+++ src/dal/drug-interaction/drug-interaction.entity.ts
@@ -0,0 +1,22 @@
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({name:'drug_interaction'})
+export class DrugInteractionEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  generic_name: string;
+
+  @Column()
+  caution: string;
+ 
+  @Column()
+  criteria: string;
+
+  @Column()
+  effect_inr: string;
+
+
+}
