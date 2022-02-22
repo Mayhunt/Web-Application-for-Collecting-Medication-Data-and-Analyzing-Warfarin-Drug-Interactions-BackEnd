3491fdc22e5bca4e1cd6c24612cafb3c8db42be8
MeepoohSUW <99584996+MeepoohSUW@users.noreply.github.com>
2022-02-22 22:57:26 +0700

    create user table

---
+++ src/dal/user/user.entity.ts
@@ -0,0 +1,17 @@
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({name:'user'})
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
