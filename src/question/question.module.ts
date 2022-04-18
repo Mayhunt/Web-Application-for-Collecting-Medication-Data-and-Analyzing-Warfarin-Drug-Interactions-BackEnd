ca68a2aa6459e8c5e802a6fd2aa99480f779cc69
Mayhunt <kanwiya.may@gmail.com>
2022-04-19 05:42:58 +0700

    Add question module

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuestionRepository } from 'src/pkg/dal/question/question.repository';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([QuestionRepository])],
   controllers: [QuestionController],
   providers: [QuestionService],
 })
