70b5d2d1b3bbc926640f2249e705480295a19503
Mayhunt <kanwiya.may@gmail.com>
2022-05-14 12:56:47 +0700

    fw

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QuestionEntity } from 'src/pkg/dal/question/question.entity';
 import { QuestionRepository } from 'src/pkg/dal/question/question.repository';
+import { UserEntity } from 'src/pkg/dal/user/user.entity';
 import { CreateQuestionDto } from './dto/create-Question.dto';
 
 @Injectable()
@@ -13,13 +14,19 @@ export class QuestionService {
 
   async createQuestion(
     createQuestionDto: CreateQuestionDto,
+    user: UserEntity,
   ): Promise<QuestionEntity> {
     const { question, answer } = createQuestionDto;
     const ques = this.questionRepository.create({
       question,
       answer,
+      user,
     });
     await this.questionRepository.save(ques);
     return ques;
+  }
+  async getQuestions(user: UserEntity): Promise<QuestionEntity[]> {
+    const Questions = await this.questionRepository.find({ where: { user } });
+    return Questions;
   }
 }
